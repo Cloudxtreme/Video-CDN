1a4fcf4aa8188d350afd97d0b5757ac088b2cd81
Malek Anabtawi <maleka@qatar.cmu.edu>
2016-04-15 17:33:50 +0300

    Added Santa and LSA structs

--- src/ospf.h
+++ src/ospf.h
@@ -1,4 +1,16 @@
 #ifndef OSPF_H
 #define OSPF_H
 
+typedef struct santa{
+  char name[40];
+  UT_hash_handle hh;
+} santa;
+
+typedef struct lsa{
+  char sender[40];
+  int seq;
+  santa* nbors;
+  UT_hash_handle hh;
+} lsa;
+
 #endif
