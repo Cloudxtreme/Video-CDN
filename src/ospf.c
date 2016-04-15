ecbd54f8535110030b2d4fb7f29ca3a9f8a9c82e
Malek Anabtawi <maleka@qatar.cmu.edu>
2016-04-15 19:19:03 +0300

    Began parsing string

--- src/ospf.c
+++ src/ospf.c
@@ -1,1 +1,48 @@
 #include "ospf.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+extern lsa_file;
+extern servers_file;
+lsa*   lsa_hash = NULL;
+
+void is_server(char* IP, lsa* myLSA){
+	FILE* fp = fopen(servers_file, "r");
+	if(fp = NULL) return;
+
+	char* found = NULL;
+	char* line = NULL;
+	size_t len = 0;
+	ssize_t read; 
+	while((read = getline(&line, &len, fp)) != -1){
+		found = strstr(line, IP);
+		if(found != NULL){
+			myLSA->server = 1;
+			break;
+		}
+	}
+	free(fp);
+	return;
+}
+
+void parse_file(){
+	FILE* fp = fopen(lsa_file, "r");
+	if(fp = NULL) return;
+
+	char* 	line = NULL;
+	size_t 	len = 0;
+	ssize_t read; 
+	char*	IP;
+	int 	seq;
+	char*	nbors;
+	lsa*	temp;
+	while((read = getline(&line, &len, fp)) != -1){
+		sscanf(line, "%s %d %s", IP, seq, nbors);
+		temp = calloc(1, sizeof(lsa));
+		temp->sender = IP;
+		temp->seq = seq;
+		temp->nbors = ;
+	}
+}
--- src/ospf.h
+++ src/ospf.h
@@ -1,15 +1,13 @@
 #ifndef OSPF_H
 #define OSPF_H
 
-typedef struct santa{
-  char name[40];
-  UT_hash_handle hh;
-} santa;
 
 typedef struct lsa{
-  char sender[40];
-  int seq;
-  santa* nbors;
+  char 		sender[40];
+  int 		seq;
+  int 		server; 
+  int 		num_nbors;
+  char** 	nbors;
   UT_hash_handle hh;
 } lsa;
 
